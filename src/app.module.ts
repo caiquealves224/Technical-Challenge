0d0acb31fd09acc20cff1d19db5df3ab1e442d4f
caique <caiquealves224@gmail.com>
2024-10-23 22:08:39 -0300

    feat: add .env.example file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { TransferModule } from './transfer/transfer.module';
 import { ConfigModule } from '@nestjs/config';
+import { Usuario } from './usuarios/usuario.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      logging: false,
+      entities: [__dirname + '/**/*.entity{.js,.ts}'],
+      migrations: [],
+      subscribers: [],
+    }),
     UsuariosModule,
     TransferModule
   ],
---
+++ src/usuarios/usuario.entity.ts
@@ -0,0 +1,17 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+
+@Entity()
+export class Usuario {
+
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    username: string
+
+    @Column()
+    password: string
+
+    @Column()
+    birthdate: string
+}
