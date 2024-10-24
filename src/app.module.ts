2b0905575f32b288f9975d2030a689d12615f62a
caique <caiquealves224@gmail.com>
2024-10-23 22:02:56 -0300

    chore: add docker-compose file and nest/config lib

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { TransferModule } from './transfer/transfer.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsuariosModule, TransferModule],
+  imports: [
+    ConfigModule.forRoot(),
+    UsuariosModule,
+    TransferModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
